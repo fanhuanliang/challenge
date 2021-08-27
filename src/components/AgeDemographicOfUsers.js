ef0cfba1bb39b339d583e512e9534ac35c5de44f
Huanliang Fan <fanhuanliang@gmail.com>
2021-08-27 16:53:53 -0700

    Set up items state to store items. useEffect to fetch items data while component did mount.

--- src/components/AgeDemographicOfUsers.js
+++ src/components/AgeDemographicOfUsers.js
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { fetchItems } from '../apiHandler'
 
 const AgeDemographicOfUsers = () => {
-  
+  const [items, setItems] = useState([])
+
+  useEffect(() => fetchItems().then(data=>setItems(data)), []);
+  console.log(items)
   return (
     <div>
       age
