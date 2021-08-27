6bd5c764cb87157d535787c835dfcc17c8b333e7
Huanliang Fan <fanhuanliang@gmail.com>
2021-08-27 14:04:26 -0700

    Use try and catch the error if something went wrong

--- src/routes/getListOfAgesOfUsersWith.js
+++ src/routes/getListOfAgesOfUsersWith.js
@@ -3,9 +3,13 @@ const mockDBCalls = require('../database/index.js');
 
 const getListOfAgesOfUsersWithHandler = async (request, response) => {
   //In get methods sending item in the query to fetch the list
+  try {
     const itemToLookup = request.query;
     const data = await mockDBCalls.getListOfAgesOfUsersWith(itemToLookup);
     return response.status(200).send(JSON.stringify(data));
+  } catch (error) {
+    return response.status(500).send(error);
+  }
 };
 
 module.exports = (app) => {
--- src/routes/getUsers.js
+++ src/routes/getUsers.js
@@ -2,8 +2,13 @@
 const mockDBCalls = require('../database/index.js');
 
 const getUsersHandler = async (request, response) => {
+  try{
     const data = await mockDBCalls.getUsers();
     return response.status(200).send(JSON.stringify(data));
+  } catch (error){
+    return response.status(500).send(error);
+  }
+    
 };
 
 module.exports = (app) => {
