7adea3382aac6853928bfde8c7e570376992546c
Huanliang Fan <fanhuanliang@gmail.com>
2021-08-27 15:48:18 -0700

    Move to the src directory.

--- src/apiHandler.js
+++ src/apiHandler.js
@@ -1,14 +1,14 @@
-const urlGetUsers = "http://localhost:3000/getUsers";
+const urlBase = "http://localhost:3000";
 const urlGetListOfData = "http://localhost:3000/getListOfAgesOfUsersWith";
 
 //get users data
 const fetchUsers = () => {
-  return fetch(urlGetUsers).then((response) => response.json());
+  return fetch(urlBase + "/users").then((response) => response.json());
 };
 
 //get list of age of users and number of count
 const fetchAgeOfUserWith = (item) => {
-  return fetch(urlGetListOfData + `?item=${item}`).then((response) =>
+  return fetch(urlBase + `/users/age?item=${item}`).then((response) =>
     response.json()
   );
 };
