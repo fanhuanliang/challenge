df8f99883028027565270559a64c975f896cefa0
Huanliang Fan <fanhuanliang@gmail.com>
2021-08-27 13:03:14 -0700

    Implement the logic to get the requirement data list.

--- src/database/index.js
+++ src/database/index.js
@@ -24,6 +24,32 @@ const getUsers = () => {
 const getListOfAgesOfUsersWith = (item) => {
     const dataAccessMethod = () => {
         // fill me in :)
+        //resultsData will look like[{age: 90, count: 2}, {age:29, count: 1}]
+        let resultsData = [];
+        //use hash map to store the age and count
+        let map = new Map();
+        //helper function to look up the age by name
+        const getAgeByName = (userName) => {
+          for (let value of Object.values(db.usersById)) {
+            if (value.username === userName) return value.age;
+          }
+        }
+        //iterate itemsOfUserByUsername and check if the item belongs to whom
+        for(let [key, value] of Object.entries(db.itemsOfUserByUsername)) {
+          //check if user has item
+          if (value.indexOf(item) !== -1) {
+            let age= getAgeByName(key)
+            //add to map
+            if (!map.has(age)) {
+              map.set(age, 1)
+            } else {
+              map.set(age, map.get(age)+1)
+            }
+          }
+        }
+        //organize the map data.
+        map.forEach((val, key)=>resultsData.push({age: key, count: val}))
+        return resultsData;
     }
     return mockDBCall(dataAccessMethod);
 }
