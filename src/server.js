63cb816a962e652cb11fb36458ac7d30cd9a9a87
Huanliang Fan <fanhuanliang@gmail.com>
2021-08-27 11:35:08 -0700

    Serves the static html file from public folder

--- src/server.js
+++ src/server.js
@@ -2,12 +2,16 @@
 const express = require('express');
 const app = express();
 const registerRoutes = require('./routes');
+const path = require("path")
 
 // server config
 const port = process.env.PORT || 3000;
 
 // register routes
 registerRoutes(app);
+
+//serves the static html file from public folder
+app.use(express.static(path.join(__dirname, "..", "public")));
 
 // create server start method
 const start = () => {
