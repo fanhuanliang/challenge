bb1291b2545cc9523e7692d8c4a20c5722baf959
Huanliang Fan <fanhuanliang@gmail.com>
2021-08-27 16:53:10 -0700

    set up state to store the users, useEffect to fetch the data when the component mount it.

--- src/components/AllUsers.js
+++ src/components/AllUsers.js
@@ -1,13 +1,16 @@
 import React, {useState, useEffect} from 'react'
 import { fetchUsers } from '../apiHandler'
+import User from './User'
+
 const AllUsers = () => {
   const [users, setUsers] = useState([])
   
   useEffect(() => fetchUsers().then((data) => setUsers(data)), []);
-  
+  console.log(users)
   return (
     <div>
       users
+      {users.map(user=>(<User key={user.username} username={user.username} age={user.age}/>))}
     </div>
   )
 }
