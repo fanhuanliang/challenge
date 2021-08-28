5acb94a9777f620acc8f0b245157431abdc2e4a4
Huanliang Fan <fanhuanliang@gmail.com>
2021-08-27 21:43:02 -0700

    Add headings and use bootstrap to style the table

--- src/components/AllUsers.js
+++ src/components/AllUsers.js
@@ -1,6 +1,8 @@
 import React, {useState, useEffect} from 'react'
 import { fetchUsers } from '../apiHandler'
 import User from './User'
+import { Table } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css"
 
 const AllUsers = () => {
   const [users, setUsers] = useState([])
@@ -9,10 +11,23 @@ const AllUsers = () => {
   console.log(users)
   return (
     <div>
-      users
-      {users.map(user=>(<User key={user.username} username={user.username} age={user.age}/>))}
+      <h1>All Users</h1>
+      <h3>Users and their</h3>
+      <Table responsive="md" hover>
+        <thead>
+          <tr>
+            <th>Username</th>
+            <th>Age</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <User key={user.username} username={user.username} age={user.age} />
+          ))}
+        </tbody>
+      </Table>
     </div>
-  )
+  );
 }
 
 export default AllUsers
