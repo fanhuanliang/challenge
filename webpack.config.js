f85e46d5b8cbfcb261bde899135311eee9f895ef
Huanliang Fan <fanhuanliang@gmail.com>
2021-08-27 22:33:33 -0700

    Code clean up

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,6 @@ module.exports = {
             loader: "postcss-loader",
             options: {
               // `postcssOptions` is needed for postcss 8.x;
-              // if you use postcss 7.x skip the key
               postcssOptions: {
                 // postcss plugins, can be exported to postcss.config.js
                 plugins: function () {
