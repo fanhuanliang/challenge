09f3783a63d2c584382e9f63ee3e1e0856adcab6
Huanliang Fan <fanhuanliang@gmail.com>
2021-08-27 22:22:08 -0700

    Code clean up

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,14 +1,15 @@
 import React from "react";
 import AllUsers from './AllUsers'
 import AgeDemographicOfUsers from "./AgeDemographicOfUsers";
+import { Container } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const Layout = () => {
-  // console.log('layout')
   return (
-    <div>
+    <Container>
       <AllUsers />
       <AgeDemographicOfUsers />
-    </div>
+    </Container>
   );
 };
 
