77335e2fba1a9cb6e7a84dcc189f4b3955175587
Huanliang Fan <fanhuanliang@gmail.com>
2021-08-27 22:07:20 -0700

    Create function to handle the dropdown item.

--- src/components/AgeDemographicOfUsers.js
+++ src/components/AgeDemographicOfUsers.js
@@ -5,6 +5,9 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 const AgeDemographicOfUsers = () => {
   const [items, setItems] = useState([])
+  const ageOfUserWithHandler = (e) => {
+    console.log(e.target.name)
+  }
 
   useEffect(() => fetchItems().then(data=>setItems(data)), []);
   // fetchAgeOfUserWith('tv').then(data=>console.log(data));
@@ -12,12 +15,11 @@ const AgeDemographicOfUsers = () => {
   return (
     <section>
       <h1>Age Demographic of Users With ___</h1>
-      <DropdownButton
-        id="dropdown-item-button"
-        title="Items"
-      >
-        {items.map(item=>(
-          <Dropdown.Item key={item}>{item}</Dropdown.Item>
+      <DropdownButton id="dropdown-item-button" title="Items">
+        {items.map((item) => (
+          <Dropdown.Item key={item} name={item} onClick={ageOfUserWithHandler}>
+            {item}
+          </Dropdown.Item>
         ))}
       </DropdownButton>
     </section>
