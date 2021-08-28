3e031409684e46bd91f3ec3cd0746603148b5c47
Huanliang Fan <fanhuanliang@gmail.com>
2021-08-27 22:03:27 -0700

    Add heading and use react bootstrap to create Dropdown button.

--- src/components/AgeDemographicOfUsers.js
+++ src/components/AgeDemographicOfUsers.js
@@ -1,17 +1,27 @@
 import React, { useState, useEffect } from "react";
 import { fetchItems, fetchAgeOfUserWith } from "../apiHandler";
+import { DropdownButton, Dropdown } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const AgeDemographicOfUsers = () => {
   const [items, setItems] = useState([])
 
   useEffect(() => fetchItems().then(data=>setItems(data)), []);
   // fetchAgeOfUserWith('tv').then(data=>console.log(data));
-
+  console.log(items)
   return (
-    <div>
-      age
-    </div>
-  )
+    <section>
+      <h1>Age Demographic of Users With ___</h1>
+      <DropdownButton
+        id="dropdown-item-button"
+        title="Items"
+      >
+        {items.map(item=>(
+          <Dropdown.Item key={item}>{item}</Dropdown.Item>
+        ))}
+      </DropdownButton>
+    </section>
+  );
 }
 
 export default AgeDemographicOfUsers
